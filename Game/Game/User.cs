3915f226843a06336bf02f22377d54aa31b83a94
Wojciech Kalita <wojtek1311997@gmail.com>
2017-11-13 15:35:47 +0100

    some new classes

--- Game/Game/User.cs
+++ Game/Game/User.cs
@@ -8,5 +8,10 @@ namespace Game
 {
     class User
     {
+        public string name { get; private set; }
+        protected string password { get; private set; }
+        int age { get; set; }
+        string country { get; set; }
+        private User(string name, string password) { this.name = name; this.password = password; }
     }
 }
