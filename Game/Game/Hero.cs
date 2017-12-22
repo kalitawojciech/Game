8a99a91e62cb4d08fb6958776fd2e55fd35cd0aa
Wojciech Kalita <wojtek1311997@gmail.com>
2017-12-22 23:05:22 +0100

    Making fight menu in progres - Wojownik

--- Game/Game/Hero.cs
+++ Game/Game/Hero.cs
@@ -11,7 +11,7 @@ namespace Game
         protected int level = 1;
         protected int experience = 0;
         protected int experience_to_next_lvl = 100;
-        protected int aktualne_hp = 100;
+        public int aktualne_hp = 100;
         protected int max_hp = 100;
         protected int aktualna_mana = 100;
         protected int max_mana = 100;
@@ -25,26 +25,19 @@ namespace Game
         }
         protected virtual void level_up()
         {
-                Console.Clear();
-                this.level += 1;
-                this.experience = this.experience - experience_to_next_lvl;
-                experience_to_next_lvl *= (level + 2);
-                Console.WriteLine("Awansowałeś na kolejny lvl.\nAby kontynuować wciśnij dowolny przycisk.");
+            Console.Clear();
+            this.level += 1;
+            this.experience = this.experience - experience_to_next_lvl;
+            experience_to_next_lvl *= (level + 2);
+            Console.WriteLine("Awansowałeś na kolejny lvl.\nAby kontynuować wciśnij dowolny przycisk.");
         }
         public virtual void ulecz()
         {
-            if (aktualna_mana >= 30)
-            {
-                this.aktualne_hp += level * 25;
-                this.aktualna_mana -= 30;
-                Console.WriteLine("Uleczono {0} hp.\nWciśnij dowolny przycisk by kontynuować.", level * 25);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Za mało many!\nWciśnij dowolny przycisk by kontynuować.");
-                Console.ReadKey();
-            }
+            Console.Clear();
+            this.aktualne_hp += level * 25;
+            this.aktualna_mana -= 30;
+            Console.WriteLine("Uleczono {0} hp.\nWciśnij dowolny przycisk by kontynuować.", level * 25);
+            Console.ReadLine();
         }
         internal void odpocznij()
         {
@@ -73,6 +66,7 @@ namespace Game
         }
         internal override int atak()
         {
+            Console.Clear();
             int obrazenia = 0;
             obrazenia = sila * (level + 3);
             Console.WriteLine("Zadano {0} obrażeń!", sila * (level + 3));
@@ -80,6 +74,7 @@ namespace Game
         }
         internal int silny_atak()
         {
+            Console.Clear();
             int obrazenia = 0;
             obrazenia = sila * (level + 3) * 2;
             this.aktualna_mana -= 30;
@@ -88,11 +83,63 @@ namespace Game
         }
         internal int oglusz()
         {
+            Console.Clear();
             int obrazenia = 0;
             obrazenia = sila * (level + 3);
             this.aktualna_mana -= 40;
             Console.WriteLine("Zadano {0} obrażeń i ogłuszono przeciwnika!", sila * (level + 3));
             return obrazenia;
+        }
+        internal int menu()
+        {
+            ConsoleKeyInfo cki;
+            Console.Clear();
+            if (aktualna_mana >= 40)
+            {
+                Console.WriteLine("Masz {0} hp i {1} many. Co chcesz zrobić?\n1 - zwykły atak ({2} obrażeń)\n2 - silny atak ({3} obrażeń, koszt 30 many)\n3 - ogłuszenie (zadajesz {4} obrażeń oraz przeciwnik traci turę, koszt 40 many)\nPozostałe przyciski - ulecz (przywraca {5} hp, koszt 30 many)", aktualne_hp, aktualna_mana, sila * (level + 3), sila * 2 * (level + 3), sila * (level + 3), level * 25);
+                cki = Console.ReadKey();
+                if(cki.Key == ConsoleKey.D1 || cki.Key == ConsoleKey.NumPad1)
+                {
+                    return atak();
+                }
+                else if (cki.Key == ConsoleKey.D2 || cki.Key == ConsoleKey.NumPad2)
+                {
+                    return silny_atak();
+                }
+                else if (cki.Key == ConsoleKey.D3 || cki.Key == ConsoleKey.NumPad3)
+                {
+                    return oglusz();
+                }
+                else
+                {
+                    ulecz();
+                    return 0;
+                }
+            }
+            else if(aktualna_mana >= 30 && aktualna_mana < 40)
+            {
+                Console.WriteLine("Masz {0} hp i {1} many. Co chcesz zrobić?\n1 - zwykły atak ({2} obrażeń)\n2 - silny atak ({3} obrażeń, koszt 30 many)\nPozostałe przyciski - ulecz (przywraca {4} hp, koszt 30 many)", aktualne_hp, aktualna_mana, sila * (level + 3), sila * 2 * (level + 3), level * 25);
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.D1 || cki.Key == ConsoleKey.NumPad1)
+                {
+                    return atak();
+                }
+                else if (cki.Key == ConsoleKey.D2 || cki.Key == ConsoleKey.NumPad2)
+                {
+                    return silny_atak();
+                }
+                else
+                {
+                    ulecz();
+                    return 0;
+                }
+
+            }
+            return 5;
+
+
+
+
         }
 
     }
@@ -116,6 +163,7 @@ namespace Game
          }
          internal override int atak()
          {
+             Console.Clear();
              int obrazenia = 0;
              obrazenia = inteligencja * (level + 3);
              Console.WriteLine("Zadano {0} obrażeń!", inteligencja * (level  + 3));
@@ -123,6 +171,7 @@ namespace Game
          }
         internal int kula_ognia()
         {
+            Console.Clear();
             int obrazenia = 0;
             obrazenia = inteligencja * (level * 5);
             this.aktualna_mana -= 30;
@@ -131,6 +180,7 @@ namespace Game
         }
         internal int zamroź()
         {
+            Console.Clear();
             int obrazenia = 0;
             obrazenia = inteligencja * level;
             this.aktualna_mana -= 50;
@@ -138,46 +188,49 @@ namespace Game
             return obrazenia;
         }
      }
-        class Lucznik : Hero
+     class Lucznik : Hero
+     {
+         internal Lucznik() { this.aktualne_hp = 150; this.max_hp = 150; this.aktualna_mana = 150; this.max_mana = 150; this.zrecznosc = 10; }
+         protected override void level_up()
          {
-            internal Lucznik() { this.aktualne_hp = 150; this.max_hp = 150; this.aktualna_mana = 150; this.max_mana = 150; this.zrecznosc = 10; }
-            protected override void level_up()
-            {
-                if (experience >= experience_to_next_lvl)
-                {
-                     base.level_up();
-                     this.sila += level * 5;
-                     this.zrecznosc += (level + 1) * 5;
-                     this.inteligencja += level * 5;
-                     this.max_hp += max_hp * (level + 2);
-                     this.max_mana += 30;
-                     this.aktualna_mana = max_mana;
-                     this.aktualne_hp = max_hp;
-                     Console.ReadKey();
-                }
-            }
-            internal override int atak()
-            {
-                 int obrazenia = 0;
-                 obrazenia = zrecznosc * (level + 3);
-                 Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3));
-                 return obrazenia;
-            }
-            internal int head_shot()
-            {
-                int obrazenia = 0;
-                obrazenia = zrecznosc * (level + 3);
-                this.aktualna_mana -= 40;
-                Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3));
-                return obrazenia;
-            }
-            internal int mocny_strzal()
-            {
-                 int obrazenia = 0;
-                 obrazenia = zrecznosc * (level + 3) * 2;
-                 this.aktualna_mana -= 25;
-                 Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3) * 2);
-                 return obrazenia;
+             if (experience >= experience_to_next_lvl)
+             {
+                  base.level_up();
+                  this.sila += level * 5;
+                  this.zrecznosc += (level + 1) * 5;
+                  this.inteligencja += level * 5;
+                  this.max_hp += max_hp * (level + 2);
+                  this.max_mana += 30;
+                  this.aktualna_mana = max_mana;
+                  this.aktualne_hp = max_hp;
+                  Console.ReadKey();
              }
-        }
+         }
+         internal override int atak()
+         {
+             Console.Clear();
+             int obrazenia = 0;
+             obrazenia = zrecznosc * (level + 3);
+             Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3));
+             return obrazenia;
+         }
+         internal int head_shot()
+         {
+             Console.Clear();
+             int obrazenia = 0;
+             obrazenia = zrecznosc * (level + 3);
+             this.aktualna_mana -= 40;
+             Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3));
+             return obrazenia;
+         }
+         internal int mocny_strzal()
+         {
+             Console.Clear();
+             int obrazenia = 0;
+             obrazenia = zrecznosc * (level + 3) * 2;
+             this.aktualna_mana -= 25;
+             Console.WriteLine("Zadano {0} obrażeń!", zrecznosc * (level + 3) * 2);
+             return obrazenia;
+         }
+     }
 }
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -18,8 +18,25 @@ namespace Game
             karczma.karczma = karczma;
             karczma.arena = arena;
             karczma.bohater = gracz;
-            Miasto.menu(arena, karczma);
+            Wojownik enemy = new Wojownik();
+            //Miasto.menu(arena, karczma);
+            enemy.aktualne_hp = 500;
+            Console.WriteLine(enemy.aktualne_hp);
             Console.ReadKey();
+            enemy.aktualne_hp -= gracz.menu();
+            Console.WriteLine(enemy.aktualne_hp);
+
+            Console.ReadKey();
+            gracz.aktualne_hp -= enemy.menu();
+            Console.WriteLine(enemy.aktualne_hp);
+            Console.ReadKey();
+           /* while (enemy.aktualne_hp > 0)
+            {
+                enemy.aktualne_hp -= gracz.menu();
+                Console.WriteLine(enemy.aktualne_hp);
+                Console.ReadKey();
+            }
+            Console.ReadKey(); */
         }
     }
 }
